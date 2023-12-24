75a05c69da197e53e40b1fcc3d92087ebaccac8d
ahrorhaidarov <alanjon1312@gmail.com>
2023-12-24 15:17:17 +0500

    Project finished

--- my_ds_babel.py
+++ my_ds_babel.py
@@ -1,1 +1,11 @@
-# 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
+import pandas as pd
+import sqlite3 as sql
+def csv_to_sql(csv, databese, tableName):
+
+    con = sql.connect(databese)
+    df = pd.read_csv(csv)
+    df.to_sql(name=tableName, con=con, index=False, if_exists='replace')
+
+
+
+
--- my_m_and_a.py
+++ my_m_and_a.py
@@ -1,70 +1,14 @@
-import pandas as pd
-import csv
-
-gender = {'0': 'Male', '1': 'Female', 'F': 'Female', 'M': 'Male'}
-elems = {'"': '', '\\': ''}
-def filter1(csv_file):
-    df = pd.read_csv(csv_file)
-
-
-    df['City'] = df['City'].map(lambda x: x.capitalize())
-    df['Email'] = df['Email'].map(lambda x: x.lower())
-    df['LastName'] = df['LastName'].map(lambda x: x.capitalize())
-    # df['FirstName'] = df['FirstName'].map(lambda x: x.capitalize())
-    df['Country'] = df['Country'].map(lambda x: 'U.S.A')
-    # df['UserName'] = df['UserName'].map(lambda x: x.lower())
-
-    df.replace(gender, inplace=True)
-
-
-def filter2(csv_file):
-    # df = pd.read_csv(csv_file)
-    # print(df)
-    with open(csv_file, 'r') as csvfile:
-        data = {
-            'Gender': list(),
-            'FirstName': list(),
-            'LastName': list(),
-            'UserName': list(),
-            'Email': list(),
-            'Age': list(),
-            'City': list(),
-            'Country': list()
-        }
-        csvreader = csv.reader(csvfile, delimiter=';')
-        for row in csvreader:
-            data['Gender'].append(row[2])
-            data['FirstName'].append(row[3].split(' ')[0])
-            data['LastName'].append(row[3].split(' ')[1])
-            data['UserName'].append(row[3].split(' ')[0])
-            data['Email'].append(row[-1])
-            data['Age'].append(row[0])
-            data['City'].append(row[1].capitalize())
-            data['Country'].append('U.S.A')
-
-
-        df = pd.DataFrame.from_dict(data, orient='columns')
-
-        df.replace(gender, inplace=True)
-        df.replace('"', '', regex=True, inplace=True)
-        df.replace(r"\\", '', regex=True, inplace=True)
-        df.replace('years', '', regex=True, inplace=True)
-
-        df['City'] = df['City'].map(lambda x: x.capitalize())
-        df['Email'] = df['Email'].map(lambda x: x.lower())
-        df['LastName'] = df['LastName'].map(lambda x: x.capitalize())
-        df['FirstName'] = df['FirstName'].map(lambda x: x.capitalize())
-        df['Country'] = df['Country'].map(lambda x: 'U.S.A')
-        df['Age'] = df['Age'].map(lambda x: int(x[:2]))
-        df['UserName'] = df['UserName'].map(lambda x: x.lower())
-
-
-
-
+from functions import FilterContent1, FilterContent2, FilterContent3, Merge
+from my_ds_babel import csv_to_sql
 def my_m_and_a(conten1, content2, content3):
-    filter1(conten1)
-    filter2(content2)
-    # filter3(content3)
+    CleanedData1 = FilterContent1(conten1)
+    CleanedData2 = FilterContent2(content2)
+    CleanedData3 = FilterContent3(content3)
+
+    Merge(CleanedData1, CleanedData2, CleanedData3)
+
+
+
 
 my_m_and_a('only_wood_customer_us_1.csv',
            'only_wood_customer_us_2.csv',
